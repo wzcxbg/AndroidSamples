0f1195ef12041b28f28e50174f635f13841d230f
Sliver <bohong.wu@emeet.ai>
2024-02-19 18:32:30 +0800

    1.简化位图背景截取逻辑

--- app/src/main/java/com/sliver/samples/custom/CustomBlurView.kt
+++ app/src/main/java/com/sliver/samples/custom/CustomBlurView.kt
@@ -3,17 +3,12 @@ package com.sliver.samples.custom
 import android.app.Activity
 import android.content.Context
 import android.content.ContextWrapper
+import android.graphics.Bitmap
 import android.graphics.Canvas
-import android.os.Handler
-import android.os.Looper
 import android.util.AttributeSet
 import android.util.Log
 import android.view.View
-import android.view.ViewGroup
-import android.view.ViewTreeObserver.OnDrawListener
 import android.view.ViewTreeObserver.OnPreDrawListener
-import androidx.core.view.drawToBitmap
-import kotlinx.coroutines.delay
 
 class CustomBlurView : View {
     constructor(context: Context?) : super(context)
@@ -31,26 +26,12 @@ class CustomBlurView : View {
 
     override fun onDraw(canvas: Canvas) {
         if (drawEnable) {
-            super.onDraw(canvas)
             drawEnable = false
-            val parentLayout = parent as ViewGroup
-            val bitmap = parentLayout.drawToBitmap()
+            val bitmap = catchViewBackground()
             Log.e("TAG", "onDraw: ${bitmap.width} ${bitmap.height}")
-
-            val parentLocation = IntArray(2)
-            parentLayout.getLocationOnScreen(parentLocation)
-
-            val viewLocation = IntArray(2)
-            this.getLocationOnScreen(viewLocation)
-
-            val offsetX = parentLocation[0] - viewLocation[0]
-            val offsetY = parentLocation[1] - viewLocation[1]
-
-            canvas.drawBitmap(bitmap, offsetX.toFloat(), offsetY.toFloat(), null)
-
+            canvas.drawBitmap(bitmap, 0f, 0f, null)
             bitmap.recycle()
             drawEnable = true
-
 
             decorView?.viewTreeObserver?.addOnPreDrawListener(object : OnPreDrawListener {
                 override fun onPreDraw(): Boolean {
@@ -73,5 +54,26 @@ class CustomBlurView : View {
             return ctx.window.decorView
         }
         return null
+    }
+
+    private fun catchViewBackground(): Bitmap {
+        val parentLocation = IntArray(2)
+        decorView?.getLocationOnScreen(parentLocation)
+
+        val viewLocation = IntArray(2)
+        this.getLocationOnScreen(viewLocation)
+
+        val offsetX = parentLocation[0] - viewLocation[0]
+        val offsetY = parentLocation[1] - viewLocation[1]
+
+        val bitmap = Bitmap.createBitmap(
+            measuredWidth, measuredHeight,
+            Bitmap.Config.ARGB_8888
+        )
+        val canvas = Canvas(bitmap)
+        canvas.translate(-scrollX.toFloat(), -scrollY.toFloat())
+        canvas.translate(offsetX.toFloat(), offsetY.toFloat())
+        decorView?.draw(canvas)
+        return bitmap
     }
 }
