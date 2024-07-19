4071b0b7010425b98b9f128cb2489b93a350102e
Sliver <bohong.wu@emeet.ai>
2024-07-19 11:07:56 +0800

    1.解决FloatingWindowUtils的未提示所需权限的问题

--- app/src/main/java/com/sliver/samples/floatingwindow/core/FloatingWindowUtils.kt
+++ app/src/main/java/com/sliver/samples/floatingwindow/core/FloatingWindowUtils.kt
@@ -1,9 +1,11 @@
 package com.sliver.samples.floatingwindow.core
 
+import android.Manifest
 import android.content.Context
 import android.content.Intent
 import android.net.Uri
 import android.provider.Settings
+import androidx.annotation.RequiresPermission
 
 object FloatingWindowUtils {
 
@@ -11,6 +13,7 @@ object FloatingWindowUtils {
         return Settings.canDrawOverlays(context)
     }
 
+    @RequiresPermission(Manifest.permission.SYSTEM_ALERT_WINDOW)
     fun requestPermission(context: Context) {
         val intent = Intent(
             Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
