ace663eaf2e44efa7bfa9d74ee76411a987f0a8f
Sliver <bohong.wu@emeet.ai>
2024-10-12 18:23:43 +0800

    完成Shell对象封装

--- app/src/main/cpp/samples.cpp
+++ app/src/main/cpp/samples.cpp
@@ -35,30 +35,81 @@ pid_t popen2(const char *command, int *infp, int *outfp) {
 
     if (outfp == NULL)
         close(p_stdout[0]);
-    else
+    else{
         *outfp = p_stdout[0];
+//        int flags = fcntl(*outfp, F_GETFL, 0);
+//        fcntl(*outfp, F_SETFL, flags | O_NONBLOCK);
+    }
 
     return pid;
 }
+
+class Shell final {
+    pid_t pid = -1;
+    int inFd = -1;
+    int outFd = -1;
+public:
+    explicit Shell() {
+        this->pid = popen2("su", &inFd, &outFd);
+    }
+
+    std::string execute(std::string &&cmd) const {
+        std::string divider = "\xff\xfe\xfd\xfc\xfb\xfa\xf9\xf8\n";
+        std::string divider_cmd = " && echo " + divider;
+        write(inFd, cmd.c_str(), cmd.length());
+        write(inFd, divider_cmd.c_str(), divider_cmd.size());
+        std::array<char, 128> buf{};
+        std::ostringstream oss;
+        while (true) {
+            int len = read(outFd, buf.data(), buf.size());
+            if (len > 0) {
+                std::string line(buf.data(), len);
+                oss << line;
+                auto ouput = oss.str();
+                if (ouput.size() < divider.size()) {
+                    continue;
+                }
+                auto end = ouput.substr(ouput.length() - divider.size(), divider.size());
+                if (end == divider) {
+                    break;
+                }
+            }
+        }
+        return oss.str();
+    }
+
+    ~Shell() {
+        close(inFd);
+        close(outFd);
+        waitpid(pid, nullptr, 0);
+    }
+};
 
 
 extern "C"
 JNIEXPORT void JNICALL
 Java_com_sliver_samples_MainActivity_screenCapture(JNIEnv *env, jobject thiz) {
-    int infp, outfp;
-    char buf[128];
+    Shell shell;
+    std::string ret1 = shell.execute("ifconfig");
+    __android_log_print(ANDROID_LOG_ERROR, "COMMAND", "ret1: %s", ret1.c_str());
 
-    //way1：sh->su->sh
-    //查看子进程：ps -A --sort=STIME
-    pid_t pid = popen2("su\n", &infp, &outfp);
-    __android_log_print(ANDROID_LOG_ERROR, "COMMAND", "pid: %d", pid);
+    std::string ret2 = shell.execute("screencap -p");
+    __android_log_print(ANDROID_LOG_ERROR, "COMMAND", "ret2: %s %d", ret2.c_str(), ret2.size());
 
-    write(infp, "input tap 540 1000\n", sizeof( "input tap 540 1000\n"));
-    sleep(5);
-    write(infp, "input tap 540 1000\n", sizeof( "input tap 540 1000\n"));
-
-    close(infp);
-    close(outfp);
-    pid_t wait_pid = waitpid(pid, nullptr, 0);
-    __android_log_print(ANDROID_LOG_ERROR, "COMMAND", "wait_pid: %d", wait_pid);
+//    int infp, outfp;
+//    char buf[128];
+//
+//    //way1：sh->su->sh
+//    //查看子进程：ps -A --sort=STIME
+//    pid_t pid = popen2("su\n", &infp, &outfp);
+//    __android_log_print(ANDROID_LOG_ERROR, "COMMAND", "pid: %d", pid);
+//
+//    write(infp, "input tap 540 1000\n", sizeof("input tap 540 1000\n"));
+//    sleep(5);
+//    write(infp, "input tap 540 1000\n", sizeof("input tap 540 1000\n"));
+//
+//    close(infp);
+//    close(outfp);
+//    pid_t wait_pid = waitpid(pid, nullptr, 0);
+//    __android_log_print(ANDROID_LOG_ERROR, "COMMAND", "wait_pid: %d", wait_pid);
 }
