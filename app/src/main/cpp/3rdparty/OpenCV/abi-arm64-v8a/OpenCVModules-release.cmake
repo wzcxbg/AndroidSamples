329800d4cc15444d27f083496915d83b4dfe9725
Sliver <bohong.wu@emeet.ai>
2024-10-17 16:01:21 +0800

    更新opencv库

--- app/src/main/cpp/3rdparty/OpenCV/abi-arm64-v8a/OpenCVModules-release.cmake
+++ app/src/main/cpp/3rdparty/OpenCV/abi-arm64-v8a/OpenCVModules-release.cmake
@@ -15,6 +15,16 @@ set_target_properties(opencv_core PROPERTIES
 list(APPEND _cmake_import_check_targets opencv_core )
 list(APPEND _cmake_import_check_files_for_opencv_core "${_IMPORT_PREFIX}/libs/arm64-v8a/libopencv_core.so" )
 
+# Import target "opencv_flann" for configuration "Release"
+set_property(TARGET opencv_flann APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(opencv_flann PROPERTIES
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/libs/arm64-v8a/libopencv_flann.so"
+  IMPORTED_SONAME_RELEASE "libopencv_flann.so"
+  )
+
+list(APPEND _cmake_import_check_targets opencv_flann )
+list(APPEND _cmake_import_check_files_for_opencv_flann "${_IMPORT_PREFIX}/libs/arm64-v8a/libopencv_flann.so" )
+
 # Import target "opencv_imgproc" for configuration "Release"
 set_property(TARGET opencv_imgproc APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
 set_target_properties(opencv_imgproc PROPERTIES
@@ -24,6 +34,126 @@ set_target_properties(opencv_imgproc PROPERTIES
 
 list(APPEND _cmake_import_check_targets opencv_imgproc )
 list(APPEND _cmake_import_check_files_for_opencv_imgproc "${_IMPORT_PREFIX}/libs/arm64-v8a/libopencv_imgproc.so" )
+
+# Import target "opencv_ml" for configuration "Release"
+set_property(TARGET opencv_ml APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(opencv_ml PROPERTIES
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/libs/arm64-v8a/libopencv_ml.so"
+  IMPORTED_SONAME_RELEASE "libopencv_ml.so"
+  )
+
+list(APPEND _cmake_import_check_targets opencv_ml )
+list(APPEND _cmake_import_check_files_for_opencv_ml "${_IMPORT_PREFIX}/libs/arm64-v8a/libopencv_ml.so" )
+
+# Import target "opencv_photo" for configuration "Release"
+set_property(TARGET opencv_photo APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(opencv_photo PROPERTIES
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/libs/arm64-v8a/libopencv_photo.so"
+  IMPORTED_SONAME_RELEASE "libopencv_photo.so"
+  )
+
+list(APPEND _cmake_import_check_targets opencv_photo )
+list(APPEND _cmake_import_check_files_for_opencv_photo "${_IMPORT_PREFIX}/libs/arm64-v8a/libopencv_photo.so" )
+
+# Import target "opencv_dnn" for configuration "Release"
+set_property(TARGET opencv_dnn APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(opencv_dnn PROPERTIES
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/libs/arm64-v8a/libopencv_dnn.so"
+  IMPORTED_SONAME_RELEASE "libopencv_dnn.so"
+  )
+
+list(APPEND _cmake_import_check_targets opencv_dnn )
+list(APPEND _cmake_import_check_files_for_opencv_dnn "${_IMPORT_PREFIX}/libs/arm64-v8a/libopencv_dnn.so" )
+
+# Import target "opencv_features2d" for configuration "Release"
+set_property(TARGET opencv_features2d APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(opencv_features2d PROPERTIES
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/libs/arm64-v8a/libopencv_features2d.so"
+  IMPORTED_SONAME_RELEASE "libopencv_features2d.so"
+  )
+
+list(APPEND _cmake_import_check_targets opencv_features2d )
+list(APPEND _cmake_import_check_files_for_opencv_features2d "${_IMPORT_PREFIX}/libs/arm64-v8a/libopencv_features2d.so" )
+
+# Import target "opencv_imgcodecs" for configuration "Release"
+set_property(TARGET opencv_imgcodecs APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(opencv_imgcodecs PROPERTIES
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/libs/arm64-v8a/libopencv_imgcodecs.so"
+  IMPORTED_SONAME_RELEASE "libopencv_imgcodecs.so"
+  )
+
+list(APPEND _cmake_import_check_targets opencv_imgcodecs )
+list(APPEND _cmake_import_check_files_for_opencv_imgcodecs "${_IMPORT_PREFIX}/libs/arm64-v8a/libopencv_imgcodecs.so" )
+
+# Import target "opencv_videoio" for configuration "Release"
+set_property(TARGET opencv_videoio APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(opencv_videoio PROPERTIES
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/libs/arm64-v8a/libopencv_videoio.so"
+  IMPORTED_SONAME_RELEASE "libopencv_videoio.so"
+  )
+
+list(APPEND _cmake_import_check_targets opencv_videoio )
+list(APPEND _cmake_import_check_files_for_opencv_videoio "${_IMPORT_PREFIX}/libs/arm64-v8a/libopencv_videoio.so" )
+
+# Import target "opencv_calib3d" for configuration "Release"
+set_property(TARGET opencv_calib3d APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(opencv_calib3d PROPERTIES
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/libs/arm64-v8a/libopencv_calib3d.so"
+  IMPORTED_SONAME_RELEASE "libopencv_calib3d.so"
+  )
+
+list(APPEND _cmake_import_check_targets opencv_calib3d )
+list(APPEND _cmake_import_check_files_for_opencv_calib3d "${_IMPORT_PREFIX}/libs/arm64-v8a/libopencv_calib3d.so" )
+
+# Import target "opencv_highgui" for configuration "Release"
+set_property(TARGET opencv_highgui APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(opencv_highgui PROPERTIES
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/libs/arm64-v8a/libopencv_highgui.so"
+  IMPORTED_SONAME_RELEASE "libopencv_highgui.so"
+  )
+
+list(APPEND _cmake_import_check_targets opencv_highgui )
+list(APPEND _cmake_import_check_files_for_opencv_highgui "${_IMPORT_PREFIX}/libs/arm64-v8a/libopencv_highgui.so" )
+
+# Import target "opencv_objdetect" for configuration "Release"
+set_property(TARGET opencv_objdetect APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(opencv_objdetect PROPERTIES
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/libs/arm64-v8a/libopencv_objdetect.so"
+  IMPORTED_SONAME_RELEASE "libopencv_objdetect.so"
+  )
+
+list(APPEND _cmake_import_check_targets opencv_objdetect )
+list(APPEND _cmake_import_check_files_for_opencv_objdetect "${_IMPORT_PREFIX}/libs/arm64-v8a/libopencv_objdetect.so" )
+
+# Import target "opencv_stitching" for configuration "Release"
+set_property(TARGET opencv_stitching APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(opencv_stitching PROPERTIES
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/libs/arm64-v8a/libopencv_stitching.so"
+  IMPORTED_SONAME_RELEASE "libopencv_stitching.so"
+  )
+
+list(APPEND _cmake_import_check_targets opencv_stitching )
+list(APPEND _cmake_import_check_files_for_opencv_stitching "${_IMPORT_PREFIX}/libs/arm64-v8a/libopencv_stitching.so" )
+
+# Import target "opencv_video" for configuration "Release"
+set_property(TARGET opencv_video APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(opencv_video PROPERTIES
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/libs/arm64-v8a/libopencv_video.so"
+  IMPORTED_SONAME_RELEASE "libopencv_video.so"
+  )
+
+list(APPEND _cmake_import_check_targets opencv_video )
+list(APPEND _cmake_import_check_files_for_opencv_video "${_IMPORT_PREFIX}/libs/arm64-v8a/libopencv_video.so" )
+
+# Import target "opencv_gapi" for configuration "Release"
+set_property(TARGET opencv_gapi APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(opencv_gapi PROPERTIES
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/libs/arm64-v8a/libopencv_gapi.so"
+  IMPORTED_SONAME_RELEASE "libopencv_gapi.so"
+  )
+
+list(APPEND _cmake_import_check_targets opencv_gapi )
+list(APPEND _cmake_import_check_files_for_opencv_gapi "${_IMPORT_PREFIX}/libs/arm64-v8a/libopencv_gapi.so" )
 
 # Commands beyond this point should not need to know the version.
 set(CMAKE_IMPORT_FILE_VERSION)
--- app/src/main/cpp/3rdparty/OpenCV/abi-arm64-v8a/OpenCVModules.cmake
+++ app/src/main/cpp/3rdparty/OpenCV/abi-arm64-v8a/OpenCVModules.cmake
@@ -19,7 +19,7 @@ set(CMAKE_IMPORT_FILE_VERSION 1)
 set(_cmake_targets_defined "")
 set(_cmake_targets_not_defined "")
 set(_cmake_expected_targets "")
-foreach(_cmake_expected_target IN ITEMS opencv_core opencv_imgproc)
+foreach(_cmake_expected_target IN ITEMS opencv_core opencv_flann opencv_imgproc opencv_ml opencv_photo opencv_dnn opencv_features2d opencv_imgcodecs opencv_videoio opencv_calib3d opencv_highgui opencv_objdetect opencv_stitching opencv_video opencv_gapi)
   list(APPEND _cmake_expected_targets "${_cmake_expected_target}")
   if(TARGET "${_cmake_expected_target}")
     list(APPEND _cmake_targets_defined "${_cmake_expected_target}")
@@ -49,9 +49,6 @@ unset(_cmake_expected_targets)
 # Compute the installation prefix relative to this file.
 get_filename_component(_IMPORT_PREFIX "${CMAKE_CURRENT_LIST_FILE}" PATH)
 get_filename_component(_IMPORT_PREFIX "${_IMPORT_PREFIX}" PATH)
-#get_filename_component(_IMPORT_PREFIX "${_IMPORT_PREFIX}" PATH)
-#get_filename_component(_IMPORT_PREFIX "${_IMPORT_PREFIX}" PATH)
-#get_filename_component(_IMPORT_PREFIX "${_IMPORT_PREFIX}" PATH)
 if(_IMPORT_PREFIX STREQUAL "/")
   set(_IMPORT_PREFIX "")
 endif()
@@ -59,11 +56,102 @@ endif()
 # Create imported target opencv_core
 add_library(opencv_core SHARED IMPORTED)
 
+# Create imported target opencv_flann
+add_library(opencv_flann SHARED IMPORTED)
+
+set_target_properties(opencv_flann PROPERTIES
+  INTERFACE_LINK_LIBRARIES "opencv_core;opencv_core"
+)
+
 # Create imported target opencv_imgproc
 add_library(opencv_imgproc SHARED IMPORTED)
 
 set_target_properties(opencv_imgproc PROPERTIES
   INTERFACE_LINK_LIBRARIES "opencv_core;opencv_core"
+)
+
+# Create imported target opencv_ml
+add_library(opencv_ml SHARED IMPORTED)
+
+set_target_properties(opencv_ml PROPERTIES
+  INTERFACE_LINK_LIBRARIES "opencv_core;opencv_core"
+)
+
+# Create imported target opencv_photo
+add_library(opencv_photo SHARED IMPORTED)
+
+set_target_properties(opencv_photo PROPERTIES
+  INTERFACE_LINK_LIBRARIES "opencv_core;opencv_imgproc;opencv_core;opencv_imgproc"
+)
+
+# Create imported target opencv_dnn
+add_library(opencv_dnn SHARED IMPORTED)
+
+set_target_properties(opencv_dnn PROPERTIES
+  INTERFACE_LINK_LIBRARIES "opencv_core;opencv_imgproc;opencv_core;opencv_imgproc"
+)
+
+# Create imported target opencv_features2d
+add_library(opencv_features2d SHARED IMPORTED)
+
+set_target_properties(opencv_features2d PROPERTIES
+  INTERFACE_LINK_LIBRARIES "opencv_core;opencv_flann;opencv_imgproc;opencv_core;opencv_flann;opencv_imgproc"
+)
+
+# Create imported target opencv_imgcodecs
+add_library(opencv_imgcodecs SHARED IMPORTED)
+
+set_target_properties(opencv_imgcodecs PROPERTIES
+  INTERFACE_LINK_LIBRARIES "opencv_core;opencv_imgproc;opencv_core;opencv_imgproc"
+)
+
+# Create imported target opencv_videoio
+add_library(opencv_videoio SHARED IMPORTED)
+
+set_target_properties(opencv_videoio PROPERTIES
+  INTERFACE_LINK_LIBRARIES "opencv_core;opencv_imgproc;opencv_imgcodecs;opencv_core;opencv_imgproc;opencv_imgcodecs"
+)
+
+# Create imported target opencv_calib3d
+add_library(opencv_calib3d SHARED IMPORTED)
+
+set_target_properties(opencv_calib3d PROPERTIES
+  INTERFACE_LINK_LIBRARIES "opencv_core;opencv_flann;opencv_imgproc;opencv_features2d;opencv_core;opencv_flann;opencv_imgproc;opencv_features2d"
+)
+
+# Create imported target opencv_highgui
+add_library(opencv_highgui SHARED IMPORTED)
+
+set_target_properties(opencv_highgui PROPERTIES
+  INTERFACE_LINK_LIBRARIES "opencv_core;opencv_imgproc;opencv_imgcodecs;opencv_videoio;opencv_core;opencv_imgproc;opencv_imgcodecs;opencv_videoio"
+)
+
+# Create imported target opencv_objdetect
+add_library(opencv_objdetect SHARED IMPORTED)
+
+set_target_properties(opencv_objdetect PROPERTIES
+  INTERFACE_LINK_LIBRARIES "opencv_core;opencv_flann;opencv_imgproc;opencv_dnn;opencv_features2d;opencv_calib3d;opencv_core;opencv_flann;opencv_imgproc;opencv_dnn;opencv_features2d;opencv_calib3d"
+)
+
+# Create imported target opencv_stitching
+add_library(opencv_stitching SHARED IMPORTED)
+
+set_target_properties(opencv_stitching PROPERTIES
+  INTERFACE_LINK_LIBRARIES "opencv_core;opencv_flann;opencv_imgproc;opencv_features2d;opencv_calib3d;opencv_core;opencv_flann;opencv_imgproc;opencv_features2d;opencv_calib3d"
+)
+
+# Create imported target opencv_video
+add_library(opencv_video SHARED IMPORTED)
+
+set_target_properties(opencv_video PROPERTIES
+  INTERFACE_LINK_LIBRARIES "opencv_core;opencv_flann;opencv_imgproc;opencv_dnn;opencv_features2d;opencv_calib3d;opencv_core;opencv_flann;opencv_imgproc;opencv_dnn;opencv_features2d;opencv_calib3d"
+)
+
+# Create imported target opencv_gapi
+add_library(opencv_gapi SHARED IMPORTED)
+
+set_target_properties(opencv_gapi PROPERTIES
+  INTERFACE_LINK_LIBRARIES "opencv_core;opencv_flann;opencv_imgproc;opencv_dnn;opencv_features2d;opencv_calib3d;opencv_video;opencv_core;opencv_flann;opencv_imgproc;opencv_dnn;opencv_features2d;opencv_calib3d;opencv_video"
 )
 
 # Load information for each installed configuration.
