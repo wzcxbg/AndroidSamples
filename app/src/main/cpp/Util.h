b113d42f5abdca5b482847f850f740ba85c38562
Sliver <bohong.wu@emeet.ai>
2024-11-06 14:47:18 +0800

    封装文本识别

--- app/src/main/cpp/Util.h
+++ app/src/main/cpp/Util.h
@@ -8,6 +8,163 @@
 #include "include/preprocess_op.h"
 #include "models.h"
 
+class TextRecognizer {
+
+    std::vector<float> mean_ = {0.5f, 0.5f, 0.5f};
+    std::vector<float> scale_ = {1 / 0.5f, 1 / 0.5f, 1 / 0.5f};
+    bool is_scale_ = true;
+    std::string precision_ = "fp32";
+    int rec_batch_num_ = 6;
+    int rec_img_h_ = 32;        //48
+    int rec_img_w_ = 320;       //320
+    std::vector<int> rec_image_shape_ = {3, rec_img_h_, rec_img_w_};
+    // pre-process
+    PaddleOCR::CrnnResizeImg resize_op_;
+    PaddleOCR::Normalize normalize_op_;
+    PaddleOCR::PermuteBatch permute_op_;
+
+    Ort::Env env = Ort::Env(ORT_LOGGING_LEVEL_WARNING, "rec");
+    std::unique_ptr<Ort::Session> session = {nullptr};
+
+    std::vector<std::string> label_list_;
+
+public:
+    explicit TextRecognizer() {
+        Ort::SessionOptions session_options;
+        session = std::make_unique<Ort::Session>(
+                env, rec_onnx.data(), rec_onnx.size_bytes(),
+                session_options);
+        this->rec_img_h_ = 48;
+        this->rec_img_w_ = 320;
+        this->rec_image_shape_ = {3, 48, 320};
+        this->label_list_ = ReadDict();
+    }
+
+    void Run(std::vector<cv::Mat> img_list,
+             std::vector<std::string> &rec_texts,
+             std::vector<float> &rec_text_scores) {
+        int img_num = img_list.size();
+        std::vector<float> width_list;
+        for (int i = 0; i < img_num; i++) {
+            width_list.push_back(float(img_list[i].cols) / img_list[i].rows);
+        }
+        std::vector<int> indices = PaddleOCR::Utility::argsort(width_list);
+
+        for (int beg_img_no = 0; beg_img_no < img_num;
+             beg_img_no += this->rec_batch_num_) {
+
+
+            int end_img_no = std::min(img_num, beg_img_no + this->rec_batch_num_);
+            int batch_num = end_img_no - beg_img_no;
+            int imgH = this->rec_image_shape_[1];
+            int imgW = this->rec_image_shape_[2];
+            float max_wh_ratio = imgW * 1.0 / imgH;
+            for (int ino = beg_img_no; ino < end_img_no; ino++) {
+                int h = img_list[indices[ino]].rows;
+                int w = img_list[indices[ino]].cols;
+                float wh_ratio = w * 1.0 / h;
+                max_wh_ratio = std::max(max_wh_ratio, wh_ratio);
+            }
+
+            int batch_width = imgW;
+            std::vector<cv::Mat> norm_img_batch;
+            for (int ino = beg_img_no; ino < end_img_no; ino++) {
+                cv::Mat srcimg;
+                img_list[indices[ino]].copyTo(srcimg);
+                cv::Mat resize_img;
+                this->resize_op_.Run(srcimg, resize_img, max_wh_ratio,
+                                     false, this->rec_image_shape_);
+                this->normalize_op_.Run(&resize_img, this->mean_, this->scale_,
+                                        this->is_scale_);
+                norm_img_batch.push_back(resize_img);
+                batch_width = std::max(resize_img.cols, batch_width);
+            }
+
+            std::vector<float> input(batch_num * 3 * imgH * batch_width, 0.0f);
+            this->permute_op_.Run(norm_img_batch, input.data());
+
+
+            // Inference.
+            Ort::MemoryInfo memory_info = Ort::MemoryInfo::CreateCpu(
+                    OrtDeviceAllocator, OrtMemTypeDefault);
+            std::vector<int64_t> input_shape = {batch_num, 3, imgH, batch_width};
+            Ort::Value input_tensor = Ort::Value::CreateTensor<float>(
+                    memory_info,
+                    input.data(), input.size(),
+                    input_shape.data(), input_shape.size());
+
+            Ort::AllocatorWithDefaultOptions allocator;
+            auto input_name = session->GetInputNameAllocated(0, allocator);
+            auto output_name = session->GetOutputNameAllocated(0, allocator);
+            std::vector<const char *> input_names = {input_name.get()};
+            std::vector<const char *> output_names = {output_name.get()};
+            std::vector<Ort::Value> output_tensors = session->Run(
+                    Ort::RunOptions{nullptr},
+                    input_names.data(), &input_tensor, 1,
+                    output_names.data(), 1);
+
+            auto *output_data = output_tensors[0].GetTensorMutableData<float>();
+            std::vector<int64_t> output_shape = output_tensors[0].GetTensorTypeAndShapeInfo().GetShape();
+            int out_num = std::accumulate(output_shape.begin(), output_shape.end(),
+                                          1, std::multiplies<int64_t>());
+            std::vector<float> out_data(output_data, output_data + out_num);
+
+            std::vector<int64_t> predict_shape = output_shape;
+            std::vector<float> predict_batch = out_data;
+
+
+            // ctc decode
+            for (int m = 0; m < predict_shape[0]; m++) {
+                std::string str_res;
+                int argmax_idx;
+                int last_index = 0;
+                float score = 0.f;
+                int count = 0;
+                float max_value = 0.0f;
+
+                for (int n = 0; n < predict_shape[1]; n++) {
+                    // get idx
+                    argmax_idx = int(PaddleOCR::Utility::argmax(
+                            &predict_batch[(m * predict_shape[1] + n) * predict_shape[2]],
+                            &predict_batch[(m * predict_shape[1] + n + 1) * predict_shape[2]]));
+                    // get score
+                    max_value = float(*std::max_element(
+                            &predict_batch[(m * predict_shape[1] + n) * predict_shape[2]],
+                            &predict_batch[(m * predict_shape[1] + n + 1) * predict_shape[2]]));
+
+                    if (argmax_idx > 0 && (!(n > 0 && argmax_idx == last_index))) {
+                        score += max_value;
+                        count += 1;
+                        str_res += label_list_[argmax_idx];
+                    }
+                    last_index = argmax_idx;
+                }
+                score /= count;
+                if (std::isnan(score)) {
+                    continue;
+                }
+                rec_texts[indices[beg_img_no + m]] = str_res;
+                rec_text_scores[indices[beg_img_no + m]] = score;
+            }
+        }
+    }
+
+    static std::vector<std::string> ReadDict() {
+        std::vector<std::string> result;
+        result.emplace_back("#");
+
+        std::istringstream iss;
+        iss.str(std::string(ppocr_keys_v1_txt.data(), ppocr_keys_v1_txt.size_bytes()));
+        std::string line;
+        while (getline(iss, line)) {
+            result.push_back(line);
+        }
+
+        result.emplace_back(" ");
+        return result;
+    };
+};
+
 class TextClassifier {
     std::vector<float> mean_ = {0.5f, 0.5f, 0.5f};
     std::vector<float> scale_ = {1 / 0.5f, 1 / 0.5f, 1 / 0.5f};
