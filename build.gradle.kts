66ecd7d5b8f50e4b757e034038c7703d2a1f3d60
Sliver <bohong.wu@emeet.ai>
2024-11-14 14:33:40 +0800

    add kotlinx-serialization plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,4 +4,5 @@ plugins {
     id("org.jetbrains.kotlin.android") version "2.0.21" apply false
     id("com.google.dagger.hilt.android") version "2.52" apply false
     id("com.google.devtools.ksp") version "2.0.21-1.0.26" apply false
+    id("org.jetbrains.kotlin.plugin.serialization") version "2.0.21" apply false
 }
--- sample-route-navigation/build.gradle.kts
+++ sample-route-navigation/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("org.jetbrains.kotlin.plugin.serialization")
 }
 
 android {
@@ -45,6 +46,8 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
+
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-core-jvm:1.7.3")
 
     implementation("androidx.navigation:navigation-fragment-ktx:2.8.4")
     implementation("androidx.navigation:navigation-ui-ktx:2.8.4")
